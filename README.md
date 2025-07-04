bd56bd709f668b44ed6959236128c8d66022b054
dj8yf0μl <noreply@nowhere.org>
2025-07-04 21:21:45 +0300

    chore: add sign_deploy_contract_as_global_account_id example

--- README.md
+++ README.md
@@ -151,9 +151,10 @@ cargo run --example sign_batch_all_actions_v1
 cargo run --example sign_batch_all_actions_v1  -- --speculos-test-generate
 cargo run --example sign_deploy_contract_as_global_hash
 cargo run --example sign_deploy_contract_as_global_hash  -- --speculos-test-generate
+cargo run --example sign_deploy_contract_as_global_account_id
+cargo run --example sign_deploy_contract_as_global_account_id  -- --speculos-test-generate
 ```
 
-<!-- TODO #A: add deploy_contract_as_global_account_id -->
 
 <!-- TODO #A: add deploy_contract_use_global_hash -->
 
---
+++ examples/sign_transaction/deploy_contract_as_global_account_id.rs
@@ -0,0 +1,30 @@
+use near_ledger::NEARLedgerError;
+use near_primitives_core::hash::CryptoHash;
+
+#[path = "../common/lib.rs"]
+mod common;
+
+fn tx(ledger_pub_key: ed25519_dalek::VerifyingKey) -> near_primitives::transaction::Transaction {
+    let mut tx = common::tx_template(ledger_pub_key);
+
+    let code = core::iter::repeat(42u8).take(3000).collect::<Vec<_>>();
+
+    let code_hash = CryptoHash::hash_bytes(&code);
+    log::info!("Contract code hash: {:?}", code_hash);
+    let action = near_primitives::transaction::Action::DeployGlobalContract(near_primitives::action::DeployGlobalContractAction {
+        code: std::sync::Arc::from(code),
+        deploy_mode: near_primitives::action::GlobalContractDeployMode::AccountId,
+        
+    });
+    log::warn!("action bytes: {:x?}", borsh::to_vec(&action).expect("no ser err"));
+    tx.actions = vec![action];
+    near_primitives::transaction::Transaction::V0(tx)
+}
+
+fn main() -> Result<(), NEARLedgerError> {
+    // TODO #F: replace with actual signature from test after test passes and stops
+    // resulting in `TxParsingFail = 0xB005`
+    let result_signature_from_speculos_test = hex::decode("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+
+    common::get_key_sign_and_verify_flow_with_cli_parse(tx, result_signature_from_speculos_test)
+}
